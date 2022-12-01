bae4b45f185cfebe49c5b32d2a77026da19c9b76
Ozenilson Cruz <59063314+ozenilsoncruz@users.noreply.github.com>
2022-12-01 11:29:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -17,9 +17,35 @@
 #define MQTT_PUBLISH_TOPIC     "sbc/sensores"
 #define MQTT_SUBSCRIBE_TOPIC   "node/+/sensores/#"
 
+
+// Variables will change:
+int buttonState;                       
+int lastButtonState = 1;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
+void btn_press(int buttonPin){
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == LOW) {
+       printf("Pressionado");
+      }
+    }
+  }
+  lastButtonState = reading;
+}
+
+
 int main() {
     wiringPiSetup();
-    //mqtt_config();  // configura o mqtt
+    mqtt_config();  // configura o mqtt
     initDisplay();  // inicializa o display lcd
 
     // define os botoes como modo de entrada
@@ -30,9 +56,9 @@ int main() {
     write_textLCD("   Problema 3    ", "      MQTT     ");
    
     while(1){
-       if(digitalRead(botao_1) == 0){
-          printf("Hello, World!");
-       }
+       btn_press(botao_1);
+       btn_press(botao_2);
+       btn_press(botao_3);
     }
 
     return 0;
